12f1feb54ba7e22de392b03b199e2f1aeb3dc360
Jakob Hilden <jakobhilden@gmail.com>
2012-01-12 07:46:11 +0100

    gems (e.g. coffee-script) and corrected pusher.js url

--- traxel_pipe.rb
+++ traxel_pipe.rb
@@ -4,6 +4,10 @@ get '/' do
   haml :index
 end
 
+get '/application.js' do
+  coffee :application
+end
+
 get '/ping' do
   Pusher['test_channel'].trigger('greet', {
     :greeting => "Hello there!"
